5dcafd4b9c0fd7edc2f48c624d4ad2209a7e71ce
Karol Kowalski <ordon1313@gmail.com>
2018-04-24 10:43:31 +0200

    Extract AMOUNT_SEND_TO_EACH_ADDRESS_IN_WEI

--- src/main/java/io/userfeeds/airdrop/components/KovanAddressProcessor.kt
+++ src/main/java/io/userfeeds/airdrop/components/KovanAddressProcessor.kt
@@ -5,31 +5,23 @@ import io.userfeeds.airdrop.processing.AddressProcessing
 import io.userfeeds.cache.util.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import org.web3j.abi.Utils
-import org.web3j.abi.datatypes.DynamicArray
-import org.web3j.abi.datatypes.Function
-import org.web3j.abi.datatypes.Utf8String
-import org.web3j.abi.datatypes.generated.Bytes20
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.Web3j
-import org.web3j.protocol.core.RemoteCall
-import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.protocol.http.HttpService
-import org.web3j.tx.Contract
 import java.math.BigInteger
-import java.util.*
 
 @Component
 class KovanAddressProcessor(
         @Value("\${PRIVATE_KEY}") private val privateKey: String,
         @Value("\${AIRDROP_CLAIM_ID}") private val airdropClaimId: String,
         @Value("\${INFURA_TOKEN}") private val infuraToken: String,
-        @Value("\${PROCESS_ADDRESSES}") private val processAddresses: Boolean
+        @Value("\${PROCESS_ADDRESSES}") private val processAddresses: Boolean,
+        @Value("\${AMOUNT_SEND_TO_EACH_ADDRESS_IN_WEI}") amountSendToEachAddressInWei: String
 ) : AddressProcessing.Processor {
 
     private val logger = logger()
 
-    private val weiSendToEachAddress = 1_000_000_000L
+    private val weiSendToEachAddress = BigInteger(amountSendToEachAddressInWei)
     private val contractAddress = "0x5301F5b1Af6f00A61E3a78A9609d1D143B22BB8d"
     private val web3j = Web3j.build(HttpService("https://kovan.infura.io/$infuraToken"))
     private val contract = AirdropContract.load(
@@ -50,7 +42,7 @@ class KovanAddressProcessor(
             addresses.map { it.hexStringToByteArray() }
 
     private fun getTotalWeiValue(addresses: List<String>) =
-            BigInteger.valueOf(weiSendToEachAddress).times(BigInteger.valueOf(addresses.size.toLong()))
+            weiSendToEachAddress.times(BigInteger.valueOf(addresses.size.toLong()))
 
     private fun String.hexStringToByteArray(): ByteArray {
         return drop(2).chunked(2)
